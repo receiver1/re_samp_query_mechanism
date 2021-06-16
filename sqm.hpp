bac7ce3de04596bfdd1261f3a8d3945834de8fcd
Receiver1 <62743649+Receiver1@users.noreply.github.com>
2021-06-16 10:17:08 +0300

    Add files via upload

---
+++ sqm.hpp
@@ -0,0 +1,115 @@
+#pragma once
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <sstream>
+#include <map>
+
+#include "wrappers.hpp"
+
+#pragma comment(lib, "Ws2_32.lib")
+
+enum e_query_type: uint8_t
+{
+	QUERYTYPE_SERVERINFO = 'i',
+	QUERYTYPE_SERVERRULES = 'r',
+	QUERYTYPE_SERVERPING = 'p',
+	QUERYTYPE_PLAYERLIST = 'c'
+};
+
+class c_sqm
+{
+	SOCKET _socket {INVALID_SOCKET};
+	sockaddr _address;
+	const char *_ip;
+	u_short _port;
+public:
+	static c_sqm *singleton()
+	{
+		static c_sqm instance;
+		return &instance;
+	}
+
+	void initialize(const char *ip, u_short port)
+	{
+		WSADATA wsa_data;
+		int result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+		if (result != 0)
+			throw std::runtime_error("Failed to initiates WinSock.");
+
+		sockaddr_in address;
+		address.sin_family = AF_INET;
+		address.sin_addr.s_addr = inet_addr(ip);
+		address.sin_port = htons(port);
+
+		_socket = socket(address.sin_family, SOCK_DGRAM, IPPROTO_UDP);
+		if (_socket == -1)
+			throw std::runtime_error("Failed to create socket.");
+
+		u_long param = 1;
+		result = ioctlsocket(_socket, FIONBIO, &param);
+		if (result < 0)
+			throw std::runtime_error("Failed to set socket non-blocking.");
+
+		_address = reinterpret_cast<sockaddr&>(address);
+		_ip = ip;
+		_port = port;
+	}
+
+	/*
+	*	Конвертирует IP в байты
+	*/
+	std::string translate_ip(std::string ip)
+	{
+		std::string result, part;
+		std::stringstream it(ip);
+		while (std::getline(it, part, '.')) {
+			result += atoi(part.c_str());
+		}
+		return result;
+	}
+
+	/*
+	*	Отправляет запрос и ждёт ответа
+	*/
+	std::stringstream query(e_query_type type, int timeout)
+	{
+		std::string packet {"SAMP"};
+
+		packet += translate_ip(_ip);
+		packet += _port & 0xFFu;
+		packet += _port >> 8 & 0xFFu;
+		packet += type;
+
+		sendto(_socket, packet.c_str(), packet.size(), 0, &_address, sizeof(_address));
+		return receive(timeout);
+	}
+
+	std::stringstream receive(int timeout)
+	{
+		char buffer[2048];
+		sockaddr from;
+		socklen_t from_length = sizeof(_address);
+
+		std::stringstream packet {};
+		time_t start_time = time(0);
+		while (time(0) - start_time < timeout)
+		{
+			Sleep(1);
+			int received_bytes = recvfrom(_socket, buffer, sizeof(buffer), 0, &from, &from_length);
+			if (received_bytes > 11) {
+				packet = std::stringstream(std::string(buffer, received_bytes));
+				start_time = time(0);
+			}
+		}
+		return packet;
+	}
+
+	template<typename T> 
+	T handle(std::stringstream ss)
+	{
+		return T {ss};
+	}
+};
---
+++ wrappers.hpp
@@ -0,0 +1,104 @@
+#pragma once
+
+class c_fast_reader 
+{
+	std::stringstream *_ss;
+public:
+	void set_stream(std::stringstream &ss)
+	{
+		_ss = &ss;
+	}
+
+	template<typename T>
+	T read_num(std::streampos pos = -1)
+	{
+		T result;
+		if (pos != -1) {
+			_ss->seekg(pos);
+		}
+		_ss->read(reinterpret_cast<char *>(&result), sizeof(T));
+		return result;
+	}
+
+	template<typename T = uint32_t>
+	std::string read_str(std::streampos pos = -1)
+	{
+		std::string result;
+		T result_length = read_num<T>();
+		result.resize(result_length);
+		_ss->read(&result[0], result_length);
+		return result;
+	}
+};
+
+struct s_server_info : private c_fast_reader
+{
+	uint8_t is_locked;
+	uint16_t players;
+	uint16_t max_players;
+	std::string host_name;
+	std::string game_mode;
+	std::string language;
+
+	s_server_info(std::stringstream &ss)
+	{
+		ss.seekg(11);
+		set_stream(ss);
+		is_locked = read_num<uint8_t>();
+		players = read_num<uint16_t>();
+		max_players = read_num<uint16_t>();
+		host_name = read_str();
+		game_mode = read_str();
+		language = read_str();
+	}
+};
+
+struct s_server_ping : private c_fast_reader
+{
+	uint8_t first_ping, second_ping, third_ping, fourth_ping;
+
+	s_server_ping(std::stringstream &ss)
+	{
+		ss.seekg(11);
+		set_stream(ss);
+		first_ping = read_num<uint8_t>();
+		second_ping = read_num<uint8_t>();
+		third_ping = read_num<uint8_t>();
+		fourth_ping = read_num<uint8_t>();
+	}
+};
+
+struct s_server_rules : private c_fast_reader
+{
+	std::map<std::string, std::string> rules;
+
+	s_server_rules(std::stringstream &ss)
+	{
+		ss.seekg(11);
+		set_stream(ss);
+		uint16_t rule_count = read_num<uint16_t>();
+		for (uint16_t i = 0; i < rule_count; ++i) {
+			std::string rule, value;
+			rule = read_str<uint8_t>();
+			value = read_str<uint8_t>();
+			rules.emplace(rule, value);
+		}
+	}
+};
+
+struct s_player_list : private c_fast_reader
+{
+	std::map<std::string, int> players;
+
+	s_player_list(std::stringstream &ss)
+	{
+		ss.seekg(11);
+		set_stream(ss);
+		uint16_t players_count = read_num<uint16_t>();
+		for (uint16_t i = 0; i < players_count; ++i) {
+			std::string name = read_str<uint8_t>();
+			int score = read_num<int>();
+			players.emplace(name, score);
+		}
+	}
+};
